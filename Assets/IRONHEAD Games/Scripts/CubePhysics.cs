2c8bb0202d3e9b0243a84c242a3fa0db8ec4ca03
Andres Bayona <al.bayona@uniandes.edu.co>
2021-09-21 00:44:58 -0500

    Cc

---
+++ Assets/IRONHEAD Games/Scripts/CubePhysics.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePhysics : MonoBehaviour
+{
+    public float magnitude;
+    public float timePeriod;
+    public float timeSinceStart;
+    public float movementSpeed;
+    public Vector3 pivot;
+    public int wave = 0;
+    private void Start()
+    {
+        pivot = transform.localPosition;
+        timeSinceStart = (3 * timePeriod) / 4;
+        wave = Random.Range(0, 2);
+     magnitude = 0.5f;
+     timePeriod = 1.1f;
+     movementSpeed = 4f;
+
+}
+    void Update()
+    {
+
+        Vector3 nextPos = transform.localPosition;
+
+        bool isBig = tag == "Big";
+        if (isBig)
+        {
+
+            float factor =  0.1f  * ((float)Random.Range(0, 1));
+            float factor2 =  0.1f  * ((float)Random.Range(0, 1));
+           float factor3 =  0.1f  * ((float)Random.Range(0, 1));
+            magnitude = magnitude * (1 + factor2);
+
+           movementSpeed = movementSpeed * (1 + factor);
+
+            timePeriod = timePeriod * (1 - factor3);
+          
+        }
+
+        if (wave == 0 || isBig)
+        {
+            nextPos.x = pivot.x + magnitude * Mathf.Sin(((Mathf.PI * 2) / timePeriod) * timeSinceStart);
+            nextPos.y = pivot.y + 0.3f *magnitude * Mathf.Cos(((Mathf.PI * 2) / timePeriod) * timeSinceStart);
+            timeSinceStart += Time.deltaTime;
+        }
+
+
+
+
+
+        nextPos += Vector3.forward * Time.deltaTime * movementSpeed;
+
+        transform.localPosition = nextPos;
+    }
+
+
+}
---
+++ Assets/IRONHEAD Games/Scripts/CubeSpawnManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class TextObject : MonoBehaviour
+{
+    public SpriteRenderer backGround;
+    public TextMesh textMesh;
+    public Color backGroundColour = Color.white;
+    public Color textColour = Color.black;
+    public string text = "AB";
+    // Use this for initialization
+
+    void Start()
+    {
+        backGround.color = backGroundColour;
+        textMesh.color = textColour;
+        textMesh.text = text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+public class CubeSpawnManager : MonoBehaviour
+{
+    public GameObject[] Spawnpoints;
+    public GameObject[] Cubeprefabs;
+    private int index;
+    private int indexcube;
+
+    public float timeRate;
+
+    void Start()
+    {
+        StartCoroutine(CreateCubes());
+    }
+
+
+    private IEnumerator CreateCubes()
+    {
+        int cont = 0;
+        while (true) { 
+
+            cont++;
+            index = Random.Range(0, 4);
+
+            int number = Random.Range(0, 500);
+            indexcube = number % 2;
+
+            bool isBig = index == 2 && indexcube == 1;
+            if (isBig) {
+                indexcube = 2;
+            }
+
+
+            GameObject cube = Instantiate(Cubeprefabs[indexcube], Spawnpoints[index].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+
+            cube.GetComponentInChildren<Text>().text = "" + number;
+
+
+
+            cube.transform.SetParent(transform);
+
+            yield return new WaitForSeconds(timeRate);
+        }
+    }
+
+}
